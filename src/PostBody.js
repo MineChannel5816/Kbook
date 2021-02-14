5042fef68c88ca92965d58ed27c94904d857fb4f
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-14 23:12:11 +0100

    Implemented dark mode

--- src/PostBody.js
+++ src/PostBody.js
@@ -1,14 +1,18 @@
 function PostBody(props) {
   return (
     <div className="divPostBody flex flex-col">
-      <p className="mr-12 mb-3 pl-4 font-medium">{props.text}</p>
+      <p className="mr-12 mb-3 pl-4 font-medium dark:text-white dark:text-opacity-75">
+        {props.text}
+      </p>
       <img
         className="p-0 max-w-5xl w-full object-cover"
         alt="ImageOfThePost"
         src={props.image}
       />
       <div>
-        <p className="float-right flex-grow m-3">Commenti: {props.comments}</p>
+        <p className="float-right flex-grow m-3 dark:text-white dark:text-opacity-75">
+          Commenti: {props.comments}
+        </p>
       </div>
     </div>
   );
