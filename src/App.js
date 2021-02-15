ead992d42917c7e827ea1c6a2cb5bcc151cc0dc8
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-15 12:06:32 +0100

    Fix CSS Rule

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Post from "./Post.js";
 function App() {
   return (
-    <div className="App bg-gray-100 dark:bg-kbook-default h-full w-full absolute">
+    <div className="App bg-gray-100 dark:bg-kbook-default h-full w-full p-5 flex justify-center">
       <Post />
     </div>
   );
