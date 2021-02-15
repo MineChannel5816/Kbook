a15ce42feaab60fb01565ebc579afe0934a00bc1
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-15 12:13:46 +0100

    Fix design comments

--- src/PostOtherComment.js
+++ src/PostOtherComment.js
@@ -2,27 +2,25 @@ import * as Icons from "heroicons-react";
 
 function PostOtherComment(props) {
   return (
-    <div className="divPostOtherComment flex items-center m-1 dark:text-white dark:text-opacity-80 max-w-5xl">
+    <div className="divPostOtherComment flex items-center m-1 dark:text-white dark:text-opacity-80 w-auto">
       <img
         src={props.imgProfile}
         alt="imgProfileComment"
         className="rounded-full max-h-10 ml-2"
       />
-      <div className="flex-grow">
-        <div className="flex flex-col">
-          <div className="divComment flex flex-col rounded-3xl bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-3 m-2 mb-0">
-            <p className="font-medium dark:text-white dark:text-opacity-95">
-              {props.name}
-            </p>
-            <p>{props.text}</p>
-          </div>
-          <div className="divActionButton flex ml-4 opacity-95">
-            <button className="font-medium">Mi piace</button>
-            <Icons.DotsCircleHorizontal className="w-2 opacity-95 mx-2" />
-            <button className="font-medium">Rispondi</button>
-            <Icons.DotsCircleHorizontal className="w-2 opacity-95 mx-2" />
-            <button className="font-medium">Condividi</button>
-          </div>
+      <div className="flex flex-col">
+        <div className="divComment flex flex-col rounded-3xl bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-3 m-2 mb-0">
+          <p className="font-medium dark:text-white dark:text-opacity-95">
+            {props.name}
+          </p>
+          <p>{props.text}</p>
+        </div>
+        <div className="divActionButton flex ml-4 opacity-95">
+          <button className="font-medium">Mi piace</button>
+          <Icons.DotsCircleHorizontal className="w-2 opacity-95 mx-2" />
+          <button className="font-medium">Rispondi</button>
+          <Icons.DotsCircleHorizontal className="w-2 opacity-95 mx-2" />
+          <button className="font-medium">Condividi</button>
         </div>
       </div>
     </div>
