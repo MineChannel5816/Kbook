245ea159ed3e77b788fa8d7e3ee021f2b64dab91
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-15 12:41:34 +0100

    Implemented multi-post

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import Post from "./Post.js";
 function App() {
   return (
-    <div className="App bg-gray-100 dark:bg-kbook-default h-full w-full p-5 flex justify-center">
+    <div className="App bg-gray-100 dark:bg-kbook-default h-full w-full p-5 flex flex-col items-center">
+      <Post />
       <Post />
     </div>
   );
