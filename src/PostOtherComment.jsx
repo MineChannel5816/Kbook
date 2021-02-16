d5b0cdb20d26e640b73f159d76f8fb6b39009cc6
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-16 12:09:09 +0100

    Added CSS Rules, shadow, fix dark mode

--- src/PostOtherComment.jsx
+++ src/PostOtherComment.jsx
@@ -4,19 +4,27 @@ function PostOtherComment(props) {
       <img
         src={props.imgProfile}
         alt="imgProfileComment"
-        className="rounded-full max-h-10 ml-2 mt-2 border border-gray-300 hover:border-gray-500 hover:opacity-95"
+        className="rounded-full max-h-10 ml-2 mt-2 border border-gray-300 hover:border-gray-500 hover:opacity-95 cursor-pointer"
       />
       <div className="flex flex-col">
-        <div className="divComment flex flex-col rounded-3xl bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-2 px-4 m-2 mb-0 ml-3">
-          <p className="font-medium dark:text-white dark:text-opacity-95">
+        <div className="divComment flex flex-col rounded-3xl bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-2 px-4 m-2 mb-0 ml-3 shadow-sm">
+          <p className="font-medium dark:text-white dark:text-opacity-95 hover:underline">
             {props.name}
           </p>
           <p className="leading-tight">{props.text}</p>
         </div>
         <div className="divActionButton flex ml-7 opacity-95">
-          <button className="font-medium mx-1">Mi piace</button>·
-          <button className="font-medium mx-1">Rispondi</button>·
-          <button className="font-medium mx-1">Condividi</button>
+          <button className="font-medium mx-1 hover:underline focus:outline-none">
+            Mi piace
+          </button>
+          ·
+          <button className="font-medium mx-1 hover:underline focus:outline-none">
+            Rispondi
+          </button>
+          ·
+          <button className="font-medium mx-1 hover:underline focus:outline-none">
+            Condividi
+          </button>
         </div>
       </div>
     </div>
--- src/PostPersonalComment.jsx
+++ src/PostPersonalComment.jsx
@@ -5,23 +5,23 @@ function PostPersonalComment(props) {
       <img
         src={props.imgProfile}
         alt="imgProfileComment"
-        className="rounded-full max-h-10 ml-3 border border-gray-300 hover:border-gray-400 hover:opacity-95"
+        className="rounded-full max-h-10 ml-3 border border-gray-300 hover:border-gray-400 hover:opacity-95 cursor-pointer"
       />
       <div className="flex-grow">
-        <div className="divPersonalComment flex rounded-full bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-2 m-2 ml-4 shadow-md">
+        <div className="divPersonalComment flex rounded-full bg-gray-100 dark:bg-gray-400 dark:bg-opacity-20 p-2 m-2 ml-4 shadow-sm dark:shadow-md">
           <input
             type="text"
             className="flex-grow bg-gray-100 dark:bg-transparent dark:text-white dark:text-opacity-85 px-2 outline-none"
             placeholder="Scrivi un commento publico..."
           ></input>
-          <button className="text-gray-400 mx-1 focus:outline-none">
-            <Icons.EmojiHappyOutline />
+          <button className="text-gray-400 mx-1 focus:outline-none ">
+            <Icons.EmojiHappyOutline className="hover:bg-gray-200 dark:hover:bg-kbook-default2 h-8 w-8 rounded-full p-1" />
           </button>
           <button className="text-gray-400 mx-1 focus:outline-none">
-            <Icons.CameraOutline />
+            <Icons.CameraOutline className="hover:bg-gray-200 dark:hover:bg-kbook-default2 h-8 w-8 rounded-full p-1" />
           </button>
           <button className="text-gray-400 mx-1 focus:outline-none">
-            <Icons.PlusCircleOutline />
+            <Icons.PlusCircleOutline className="hover:bg-gray-200 dark:hover:bg-kbook-default2 h-8 w-8 rounded-full p-1" />
           </button>
         </div>
       </div>
