693231b91563493a2e64294513653aeed19ff252
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-17 12:05:03 +0100

    Implemented Style Functions

--- src/ConversationItem.jsx
+++ src/ConversationItem.jsx
@@ -5,39 +5,49 @@ function returnRelative(date) {
   let dataJs = date;
   let dateL = DateTime.fromJSDate(dataJs);
   let stringDate = dateL.toRelative();
-  return stringDate;
+  return <p className="text-gray-600">{stringDate}</p>;
 }
 
-function returnRead(Reading) {
-  if (Number(Reading) === 1) {
+function returnRead(reading) {
+  if (Number(reading) === 1) {
     return <Icons.Bell className="text-blue-600 m-1" />;
   }
 }
 
-function returnTextRead(Reading, message) {
-  if (Number(Reading) === 0) {
+function returnTextRead(reading, message) {
+  if (Number(reading) === 0) {
     return (
-      <p className="w-40 h-6 overflow-x-hidden overflow-y-hidden">{message}</p>
+      <p className="w-44 h-6 overflow-x-hidden overflow-y-hidden text-gray-700">
+        {message}
+      </p>
     );
   } else {
     return (
-      <p className="w-40 h-6 overflow-x-hidden overflow-y-hidden font-bold">
+      <p className="w-44 h-6 overflow-x-hidden overflow-y-hidden font-bold">
         {message}
       </p>
     );
   }
 }
 
+function returnNameRead(reading, name) {
+  if (Number(reading) === 0) {
+    return <p className="font-medium">{name}</p>;
+  } else {
+    return <p className="font-bold">{name}</p>;
+  }
+}
+
 function ConversationItem(props) {
   return (
-    <div className="flex items-center max-h-14 w-full">
+    <div className="flex items-center max-h-14 w-full m-1 cursor-pointer hover:bg-gray-200 rounded-md">
       <img
         src={props.imgProfile}
         alt="ImgProfile"
-        className="h-12 w-12 rounded-full m-2"
+        className="h-12 w-12 rounded-full m-2 mr-4"
       />
       <div className="flex flex-col">
-        <p className="font-bold">{props.name}</p>
+        {returnNameRead(props.isRead, props.name)}
         {returnTextRead(props.isRead, props.lastMessage)}
       </div>
       <p>{returnRelative(props.date)}</p>
