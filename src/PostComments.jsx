0667098159665280ea2be9ac961d4b86520e1c5f
MineChannel5816 <danielefinocchiaro@keix.com>
2021-02-19 23:18:20 +0100

    Fix Function Calls

--- src/PostComments.jsx
+++ src/PostComments.jsx
@@ -1,27 +1,51 @@
+import faker from "faker";
 import PostPersonalComment from "./PostPersonalComment";
 import PostOtherComment from "./PostOtherComment";
 
 import ImgProfilo from "./img/Profilo.png";
-import ImgFriend from "./img/ProfiloFriend.png";
+
+let commments = [
+  {
+    imgProfile: faker.image.imageUrl(200, 200, null, true),
+    name: faker.name.findName(),
+    text: faker.lorem.sentences(),
+  },
+  {
+    imgProfile: faker.image.imageUrl(200, 200, null, true),
+    name: faker.name.findName(),
+    text: faker.lorem.sentences(),
+  },
+  {
+    imgProfile: faker.image.imageUrl(200, 200, null, true),
+    name: faker.name.findName(),
+    text: faker.lorem.sentences(),
+  },
+  {
+    imgProfile: faker.image.imageUrl(200, 200, null, true),
+    name: faker.name.findName(),
+    text: faker.lorem.sentences(),
+  },
+  {
+    imgProfile: faker.image.imageUrl(200, 200, null, true),
+    name: faker.name.findName(),
+    text: faker.lorem.sentences(),
+  },
+];
+
 function PostComments(props) {
   return (
     <div className="divPostComments flex flex-col">
       <PostPersonalComment imgProfile={ImgProfilo}></PostPersonalComment>
-      <PostOtherComment
-        imgProfile={ImgFriend}
-        name="Daniele Finocchiaro"
-        text="Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. "
-      ></PostOtherComment>
-      <PostOtherComment
-        imgProfile={ImgFriend}
-        name="Gabriele Finocchiaro"
-        text="Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. Più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum."
-      ></PostOtherComment>
-      <PostOtherComment
-        imgProfile={ImgFriend}
-        name="Nadia Finocchiaro"
-        text="Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. "
-      ></PostOtherComment>
+      {commments.map((comment) => {
+        return <PostOtherComment {...comment} />;
+      })}
+      <div className="flex">
+        <div className="mx-8 ml-6 mb-4 ">
+          <button className="font-medium text-gray-800 text-opacity-95 dark:text-white dark:text-opacity-85 focus:outline-none hover:underline">
+            Visualizza altri commenti
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
